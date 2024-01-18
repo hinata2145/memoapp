87000a2b027d47caed617a2d69f1541ce32c5c88
hinata2145 <hinatamaru2145@gmail.com>
2024-01-18 15:37:04 +0900

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+import { InputForm } from "./components/InputForm";
+import { TodoList } from "./components/TodoList";
+import { Title } from "./components/title";
+
+function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  return (
+    <div className="body">
+      <Title />
+      <InputForm taskList={taskList} setTaskList={setTaskList} />
+      <TodoList taskList={taskList} setTaskList={setTaskList} />
+    </div>
+  );
+}
+
+export default App;
